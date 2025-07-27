6ef4cd8afce6f0245a14c8de1efa6caf78dc0064
mah00j <mah06.hajj@gmail.com>
2025-07-27 22:31:10 +0300

    created the migrations/models/controllers/seeders/factories/Requests for the tables.

---
+++ app/Http/Controllers/OrdersPerHourController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrdersPerHour;
+use App\Http\Requests\StoreOrdersPerHourRequest;
+use App\Http\Requests\UpdateOrdersPerHourRequest;
+
+class OrdersPerHourController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrdersPerHourRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrdersPerHour $ordersPerHour)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrdersPerHour $ordersPerHour)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOrdersPerHourRequest $request, OrdersPerHour $ordersPerHour)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrdersPerHour $ordersPerHour)
+    {
+        //
+    }
+}
---
+++ database/seeders/OrdersPerHourSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrdersPerHourSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
