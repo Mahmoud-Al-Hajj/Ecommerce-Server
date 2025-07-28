27ce59eacc0b13557d6b7409f8485c51a3134df3
mah00j <mah06.hajj@gmail.com>
2025-07-28 10:15:22 +0300

    created the migration for the audit logs

--- database/migrations/2025_07_28_065447_create_webhook_logs.php
+++ database/migrations/2025_07_28_065447_create_webhook_logs.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders_per_hours', function (Blueprint $table) {
+        Schema::create('webhook_logs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orders_per_hours');
+        Schema::dropIfExists('webhook_log');
     }
 };
