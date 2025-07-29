f7c609b22700cb69b3d15e1a7737666ca5b59f90
mah00j <mah06.hajj@gmail.com>
2025-07-29 12:43:53 +0300

    organized

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\Common\AuthController;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\WebhookLogsController;
-use App\Http\Controllers\OrdersController;
-use App\Http\Controllers\ProductCategoriesController;
+use App\Http\User\Controllers\OrdersController;
+use App\Http\Common\Controllers\ProductsController;
+use App\Http\Admin\Controllers\WebhookLogsController;
+
 
 Route::get("/products", [ProductsController::class, "getAllProducts"]);
 Route::get("/product/{id}", [ProductsController::class, "getProductById"]);
